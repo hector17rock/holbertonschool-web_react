c78dc55d2d71b1fb52c66265b4ab4c0d10f9501f
Hector Soto <hector_17rock@hotmail.com>
2025-09-14 22:06:03 -0400

    TASK02

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -41,3 +41,19 @@ const teacher2: Teacher = {
 
 console.log(teacher1);
 console.log(teacher2);
+
+// Define the Directors interface that extends Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Example usage as provided in the requirements
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
